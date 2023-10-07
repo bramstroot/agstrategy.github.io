cc4e7c890b9d5bfce74ddc0c6c0e64b9377198fa
Josh Friese <jcfriese@my.waketech.edu>
2023-10-07 16:52:38 -0400

    version 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 jQuery(window).on('load', function() {
-      
+	"use strict";
     
-    // HIDE PRELAODER
-    $(".preloader").addClass("preloader-hidden");
-
+    
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader");   
+    
     // SHOW/ANIMATE ANIMATION CONTAINER
     setTimeout(function(){
 
-        $(".hero .animation-container").each(function(){
+        $("#intro .animation-container").each(function() {
 
             var e = $(this);
 
@@ -19,7 +20,7 @@ jQuery(window).on('load', function() {
 
         });
 
-    }, 900 );
+    }, 800 );
 
     
 });
@@ -29,33 +30,31 @@ jQuery(document).ready(function($) {
 	"use strict";
     
     
-    $(window).on('load', function() {
+    // ONE PAGE NAVIGATION
+	$(".navigation-main .navigation-items").onePageNav({
+		currentClass: "current",
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: ":not(.external)",
+		easing: "swing"
+	});
+    
+    
+    // SMOOTH SCROLL FOR SAME PAGE LINKS
+    $(document).on('click', 'a.smooth-scroll', function(event) {
         
-        // HIDE PRELAODER
-        $(".preloader").addClass("preloader-hidden");
-        
-        // SHOW/ANIMATE ANIMATION CONTAINER
-        setTimeout(function(){
-            
-            $(".hero .animation-container").each(function(){
+        event.preventDefault();
 
-                var e = $(this);
-
-                setTimeout(function(){
-                    
-                    e.addClass("run-animation");
-                    
-                }, e.data("animation-delay") );
-
-            });
-            
-        }, 900 );
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
         
     });
     
     
     // INIT PARALLAX PLUGIN
-    $(".hero .background-content.parallax-on").parallax({
+    $(".background-content.parallax-on").parallax({
         scalarX: 24,
         scalarY: 15,
         frictionX: 0.1,
@@ -63,56 +62,125 @@ jQuery(document).ready(function($) {
     });
     
     
-    // OPEN POPUP SEQUENCE
-    $(".open-popup").click(function(){
-        
-        $(".popup").addClass("show");
-        $(".popup").append('<div class="close-popup backface"></div>');
-        
+    // SCROLL REVEAL SETUP
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated-from-bottom", { 
+        duration: 600,
+        delay: 0,
+        origin: "bottom",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
     });
-
-    // CLOSE POPUP SEQUENCE
-    $(document).on('click', '.close-popup', function(){ 
+    
+    
+    // WORK CAROUSEL
+    $('.work-carousel').owlCarousel({
+        center: true,
+        items: 1,
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        responsive:{
+            800:{
+                items: 3,
+            },
+        }
+    });
+    
         
-        $(".popup").removeClass("show");
-        $(".popup .backface").remove();
+});
+
+
+jQuery(document).ready(function($) {
+    "use strict";
+    
+    
+    // SMOOTH SCROLL FOR SAME PAGE LINKS
+    $(document).on('click', 'a.smooth-scroll', function(event) {
+        
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
+        }, 500);
         
     });
     
     
-    // AJAX SUBSCRIBE FORM
-    $('.subscribe-form').submit(function() {
+    // // SCROLL REVEAL SETUP
+    // window.sr = ScrollReveal();
+    // sr.reveal(".scroll-animated", { 
+    //     duration: 600,
+    //     delay: 0,
+    //     origin: "left",
+    //     rotate: { x: 0, y: 0, z: 0 },
+    //     opacity: 0,
+    //     distance: "20vh",
+    //     viewFactor: 0.4,
+    //     scale: 1,
+    // });
+    
+    
+    // AJAX CONTACT FORM SUBMIT
+    $("#contact-form").submit(function(e) {
 
-        var postdata = $('.subscribe-form').serialize();
+        e.preventDefault();
+        var postdata = $(this).serialize();
 
         $.ajax({
 
-            type: 'POST',
-            url: 'assets/php/subscribe.php',
+            type: "POST",
+            url: "assets/php/contact.php",
             data: postdata,
-            dataType: 'json',
+            dataType: "json",
             success: function(json) {
 
-                $('.subscribe-form').removeClass("form-error");
+                $("#contact-form input, #contact-form textarea").removeClass("error");
 
-                if(json.valid === 0) {
-                    
-                    $('.subscribe-form').addClass("form-error");
-                    
-                } else {
+                setTimeout(function(){
 
-                    $('.subscribe-form').addClass("form-success");
-                    $('.subscribe-form input,.subscribe-form button').val('').prop('disabled', true);
+                    if (json.nameMessage !== "") {
+
+                        $("#contact-form-name").addClass("error");
+
+                    }
+
+                    if (json.emailMessage !== "") {
+
+                        $("#contact-form-email").addClass("error");
+
+                    }
+
+                    if (json.messageMessage !== "") {
+
+                        $("#contact-form-message").addClass("error");
+
+                    }
+
+                }, 10);
+
+                if (json.nameMessage === "" && json.emailMessage === "" && json.messageMessage === "") {
+
+                    $("#contact-form.error input, #contact-form.error textarea").removeClass("error");
+                    $('#contact-form').addClass("success");
+                    $('#contact-form textarea, #contact-form input').val("");
                     
+                    setTimeout(function(){
+                        
+                        $('#contact-form').removeClass("success");
+                        
+                    },4000);
+
                 }
-                
+
             }
 
         });
 
-        return false;
-
     });
-    
+
     
 });
